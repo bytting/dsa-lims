491d588af306ed1f27168603e7b58e588e590545
Dag Robole <dag.robole@gmail.com>
2019-05-15 10:52:43 +0200

    Adjusted sort order on some queries + more work on GetAssignmentPdfData

---
+++ database2.sql
@@ -0,0 +1,11 @@
+﻿use dsa_lims
+go
+
+alter proc csp_select_assignments_short
+	@instance_status_level int
+as
+	select id, name
+	from assignment
+	where instance_status_id <= @instance_status_level
+	order by create_date desc
+go
